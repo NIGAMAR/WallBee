47dab7264ef79997237f222361a2986a4233538f
Appy <arpitnigam.manit14@gmail.com>
2016-07-27 00:56:48 +0530

    app now supports palatte integeration

--- app/src/main/java/com/nigamar/wallbee/ImagesAdapter.java
+++ app/src/main/java/com/nigamar/wallbee/ImagesAdapter.java
@@ -1,14 +1,19 @@
 package com.nigamar.wallbee;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.github.florent37.glidepalette.BitmapPalette;
+import com.github.florent37.glidepalette.GlidePalette;
 
 import java.util.List;
 
@@ -36,7 +41,12 @@ public class ImagesAdapter extends RecyclerView.Adapter<ImageViewHolder> {
     public void onBindViewHolder(ImageViewHolder holder, int position) {
         Image image = imageList.get(position);
         holder.getImageTitle().setText(image.getImageName());
-        Glide.with(context).load(image.getImageUrl()).into(holder.getPic());
+        //Glide.with(context).load(image.getImageUrl()).into(holder.getPic());
+        Glide.with(context).load(image.getImageUrl()).listener(
+                GlidePalette.with(image.getImageUrl())
+                .use(GlidePalette.Profile.MUTED_DARK)
+                .intoBackground(holder.getBackground())
+        ).into(holder.getPic());
     }
 
     @Override
@@ -54,11 +64,13 @@ class ImageViewHolder extends RecyclerView.ViewHolder {
 
     private TextView imageTitle;
     private ImageView pic;
+    private LinearLayout background;
 
     public ImageViewHolder(View itemView) {
         super(itemView);
         imageTitle = (TextView) itemView.findViewById(R.id.imageTitle);
         pic = (ImageView) itemView.findViewById(R.id.pic);
+        background= (LinearLayout) itemView.findViewById(R.id.titleContainer);
     }
 
     public TextView getImageTitle() {
@@ -76,4 +88,13 @@ class ImageViewHolder extends RecyclerView.ViewHolder {
     public void setPic(ImageView pic) {
         this.pic = pic;
     }
+
+    public LinearLayout getBackground() {
+        return background;
+    }
+
+    public void setBackground(LinearLayout background) {
+        this.background = background;
+    }
+
 }
